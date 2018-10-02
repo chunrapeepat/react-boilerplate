9c9b55dba6c0150b8d03f36537632dcf63a75234
wachirawit wacharak <tauhoo_ice@hotmail.com>
2018-10-02 17:18:26 +0700

    Fix serving file error.

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ import path from "path";
 const app = express();
 
 import ssr from "./ssr.js";
-
+app.get("/client.min.js", (req, res) => {
+  res.sendfile("./src/client.min.js");
+});
 app.use(ssr);
-
-app.listen(3000);
+app.listen(8080);
